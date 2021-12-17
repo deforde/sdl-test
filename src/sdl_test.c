8aa753b96d54291dd8a7f1b1ad0cd65b1590fb98
deforde <daniel.forde001@gmail.com>
2021-12-17 14:26:53 +0200

    Spaceship can now fire projectiles.

--- src/sdl_test.c
+++ src/sdl_test.c
@@ -17,8 +17,12 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
+#define SPACESHIP_VELOCITY_PPS 320
+#define MAX_NUM_PROJECTILES 1024
+#define PROJECTILE_VELOCITY_PPS 640
 
 const char* spaceship_img = "data/ship.png";
+const char* projectile_img = "data/laser-bolts.png";
 
 enum {
     SPACESHIP_STATIONARY_1,
@@ -34,9 +38,14 @@ enum {
     SPACESHIP_SPRITES_TOTAL
 };
 
-#define SPACESHIP_VELOCITY_PPS 320
+enum {
+    PROJECTILE_1,
+    PROJECTILE_2,
+    PROJECTILE_SPRITES_TOTAL
+};
 
 SDL_Rect spaceship_sprite_quads[SPACESHIP_SPRITES_TOTAL];
+SDL_Rect projectile_sprite_quads[PROJECTILE_SPRITES_TOTAL];
 
 typedef struct {
     int32_t x;
@@ -47,8 +56,6 @@ typedef struct {
     vector_t position;
     vector_t velocity;
 
-    SDL_Texture* texture;
-
     SDL_Rect* sprite_quad;
     uint32_t sprite_scaling;
     SDL_Rect render_quad;
@@ -57,12 +64,18 @@ typedef struct {
     int32_t animation_idx;
     int32_t num_rendered_frames_per_animation_frame;
     int32_t rendered_frame_idx;
-} spaceship_t;
+} entity_t;
 
 typedef struct {
     SDL_Window* window;
     SDL_Renderer* renderer;
-    spaceship_t spaceship;
+
+    SDL_Texture* spaceship_texture;
+    entity_t spaceship;
+
+    SDL_Texture* projectile_texture;
+    entity_t projectiles[MAX_NUM_PROJECTILES];
+    size_t projectiles_count;
 } game_state_t;
 
 game_state_t state;
@@ -77,6 +90,10 @@ void update_state();
 void handle_event(const SDL_Event* const event);
 void render();
 
+SDL_Texture* load_texture(const char* const filename);
+
+void spawn_projectile();
+
 // ============================================================================
 // Function implementations
 // ============================================================================
@@ -87,60 +104,62 @@ void init()
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_1].y = 0;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_1].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_1].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_1].x = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_1].y = 0;
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_1].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_1].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_STATIONARY_1].x = 32;
     spaceship_sprite_quads[SPACESHIP_STATIONARY_1].y = 0;
     spaceship_sprite_quads[SPACESHIP_STATIONARY_1].w = 16;
     spaceship_sprite_quads[SPACESHIP_STATIONARY_1].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_1].x = 48;
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_1].y = 0;
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_1].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_1].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_1].x = 64;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_1].y = 0;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_1].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_1].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_2].x = 0;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_2].y = 24;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_2].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_LEFT_2].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_2].x = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_2].y = 24;
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_2].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_LEFT_2].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_STATIONARY_2].x = 32;
     spaceship_sprite_quads[SPACESHIP_STATIONARY_2].y = 24;
     spaceship_sprite_quads[SPACESHIP_STATIONARY_2].w = 16;
     spaceship_sprite_quads[SPACESHIP_STATIONARY_2].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_2].x = 48;
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_2].y = 24;
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_2].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_RIGHT_2].h = 24;
-
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_2].x = 64;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_2].y = 24;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_2].w = 16;
     spaceship_sprite_quads[SPACESHIP_BANK_HARD_RIGHT_2].h = 24;
 
+    projectile_sprite_quads[PROJECTILE_1].x = 0;
+    projectile_sprite_quads[PROJECTILE_1].y = 0;
+    projectile_sprite_quads[PROJECTILE_1].w = 16;
+    projectile_sprite_quads[PROJECTILE_1].h = 32;
+    projectile_sprite_quads[PROJECTILE_2].x = 16;
+    projectile_sprite_quads[PROJECTILE_2].y = 0;
+    projectile_sprite_quads[PROJECTILE_2].w = 16;
+    projectile_sprite_quads[PROJECTILE_2].h = 32;
+
     state.window = NULL;
     state.renderer = NULL;
+
+    state.spaceship_texture = NULL;
+    state.projectile_texture = NULL;
 
     state.spaceship.position.x = SCREEN_WIDTH / 2;
     state.spaceship.position.y = SCREEN_HEIGHT / 2;
     state.spaceship.velocity.x = 0;
     state.spaceship.velocity.y = 0;
-    state.spaceship.texture = NULL;
     state.spaceship.sprite_quad = NULL;
     state.spaceship.sprite_scaling = 2;
     state.spaceship.render_quad.x = 0;
@@ -151,6 +170,8 @@ void init()
     state.spaceship.animation_idx = 0;
     state.spaceship.num_rendered_frames_per_animation_frame = 4;
     state.spaceship.rendered_frame_idx = 0;
+
+    state.projectiles_count = 0;
 
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
@@ -180,25 +201,17 @@ void init()
         exit(EXIT_FAILURE);
     }
 
-    SDL_Surface* spaceship_surface = IMG_Load(spaceship_img);
-    if(spaceship_surface == NULL) {
-        fprintf(stderr, "Failed to load image from: \"%s\": %s\n", spaceship_img, IMG_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    state.spaceship.texture = SDL_CreateTextureFromSurface(state.renderer, spaceship_surface);
-    if(state.spaceship.texture == NULL) {
-        fprintf(stderr, "SDL texture could not be created: %s\n", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    SDL_FreeSurface(spaceship_surface);
+    state.spaceship_texture = load_texture(spaceship_img);
+    state.projectile_texture = load_texture(projectile_img);
 }
 
 void destroy()
 {
-    SDL_DestroyTexture(state.spaceship.texture);
-    state.spaceship.texture = NULL;
+    SDL_DestroyTexture(state.projectile_texture);
+    state.projectile_texture = NULL;
+
+    SDL_DestroyTexture(state.spaceship_texture);
+    state.spaceship_texture = NULL;
 
     SDL_DestroyRenderer(state.renderer);
     state.renderer = NULL;
@@ -219,6 +232,7 @@ void handle_event(const SDL_Event* const event)
             case SDLK_DOWN: state.spaceship.velocity.y += SPACESHIP_VELOCITY_PPS; break;
             case SDLK_LEFT: state.spaceship.velocity.x -= SPACESHIP_VELOCITY_PPS; break;
             case SDLK_RIGHT: state.spaceship.velocity.x += SPACESHIP_VELOCITY_PPS; break;
+            case SDLK_SPACE: spawn_projectile(); break;
         }
     }
     else if(event->type == SDL_KEYUP && event->key.repeat == 0) {
@@ -278,6 +292,31 @@ void update_state()
         ++state.spaceship.animation_idx;
         state.spaceship.animation_idx %= state.spaceship.num_animation_frames;
     }
+
+    // Update all projectile's positions and animations
+    for(size_t i = 0; i < state.projectiles_count; ++i) {
+        entity_t* projectile = &state.projectiles[i];
+        projectile->sprite_quad = &projectile_sprite_quads[PROJECTILE_1 + projectile->animation_idx];
+
+        projectile->position.y += (int32_t)(projectile->velocity.y * time_delta_s);
+        projectile->render_quad.y = projectile->position.y - projectile->render_quad.h / 2;
+
+        ++projectile->rendered_frame_idx;
+        if(projectile->rendered_frame_idx == projectile->num_rendered_frames_per_animation_frame) {
+            projectile->rendered_frame_idx = 0;
+            ++projectile->animation_idx;
+            projectile->animation_idx %= projectile->num_animation_frames;
+        }
+    }
+    // Remove all projectiles that have exited the screen
+    for(size_t i = 0; i < state.projectiles_count;) {
+        if(state.projectiles[i].position.y < 0) {
+            state.projectiles[i] = state.projectiles[state.projectiles_count - 1];
+            state.projectiles_count--;
+            continue;
+        }
+        ++i;
+    }
 }
 
 void render()
@@ -285,9 +324,58 @@ void render()
     SDL_SetRenderDrawColor(state.renderer, 0x0, 0x0, 0x0, 0xFF);
     SDL_RenderClear(state.renderer);
 
-    SDL_RenderCopy(state.renderer, state.spaceship.texture, state.spaceship.sprite_quad, &state.spaceship.render_quad);
+    SDL_RenderCopy(state.renderer, state.spaceship_texture, state.spaceship.sprite_quad, &state.spaceship.render_quad);
+    for(size_t i = 0; i < state.projectiles_count; ++i) {
+        entity_t* projectile = &state.projectiles[i];
+        SDL_RenderCopy(state.renderer, state.projectile_texture, projectile->sprite_quad, &projectile->render_quad);
+    }
 
     SDL_RenderPresent(state.renderer);
+}
+
+SDL_Texture* load_texture(const char* const filename)
+{
+    SDL_Surface* surface = IMG_Load(filename);
+    if(surface == NULL) {
+        fprintf(stderr, "Failed to load image from: \"%s\": %s\n", filename, IMG_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(state.renderer, surface);
+    if(texture == NULL) {
+        fprintf(stderr, "SDL texture could not be created: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_FreeSurface(surface);
+
+    return texture;
+}
+
+void spawn_projectile()
+{
+    if(state.projectiles_count < MAX_NUM_PROJECTILES) {
+        entity_t* projectile = &state.projectiles[state.projectiles_count++];
+
+        projectile->position.x = state.spaceship.position.x;
+        projectile->position.y = state.spaceship.position.y + state.spaceship.render_quad.h / 2;
+
+        projectile->velocity.x = 0;
+        projectile->velocity.y = -PROJECTILE_VELOCITY_PPS;
+
+        projectile->sprite_quad = &projectile_sprite_quads[PROJECTILE_1];
+        projectile->sprite_scaling = 2;
+
+        projectile->render_quad.w = projectile->sprite_quad->w * projectile->sprite_scaling;
+        projectile->render_quad.h = projectile->sprite_quad->h * projectile->sprite_scaling;
+        projectile->render_quad.x = projectile->position.x - projectile->render_quad.w / 2;
+        projectile->render_quad.y = projectile->position.y - projectile->render_quad.h / 2;
+
+        projectile->num_animation_frames = 2;
+        projectile->animation_idx = 0;
+        projectile->num_rendered_frames_per_animation_frame = 4;
+        projectile->rendered_frame_idx = 0;
+    }
 }
 
 // ============================================================================
