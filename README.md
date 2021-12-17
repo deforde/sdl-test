6114264c89a66d808f91a673b05197666046954d
deforde <daniel.forde001@gmail.com>
2021-12-17 10:49:51 +0200

    Switch to using .png files for graphics content.

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Playing with [SDL](https://www.libsdl.org/).
 
 ---------------------------------------------------
 
-## Building
+## Building and Running
 
 The current build configuration is intended to target Windows only (for now).
 ```
@@ -15,8 +15,13 @@ cmake ..
 cmake --build . --config Release --target ALL_BUILD
 ```
 
-Expects environment variables: `SDL_INCLUDE` and `SDL_LIB` to be defined, and point to the include and lib directories for SDL respectively.
-The produced executable will need access to `SDL2.dll` in order to run as well.
+Expects the following environment variables to be defined:
+- `SDL_INCLUDE`: The path to the SDL2 include directory.
+- `SDL_LIB`: The path to the SDL2 lib directory.
+- `SDL_IMAGE_INCLUDE`: The path to the SDL Image extension include directory.
+- `SDL_LIB`: The path to the SDL Image extension lib directory.
+
+The executable will require access to the various `SDL2` and `SDL2_image` dlls at runtime.
 
 ---------------------------------------------------
 
@@ -24,3 +29,5 @@ The produced executable will need access to `SDL2.dll` in order to run as well.
 
 - [SDL Development Libs](https://www.libsdl.org/release/SDL2-devel-2.0.18-VC.zip) (for building from source)
 - [SDL Runtime Binaries](https://www.libsdl.org/release/SDL2-2.0.18-win32-x64.zip) (for distribution of pre-compiled binary)
+- [SDL Image Development Libs](https://www.libsdl.org/projects/SDL_image/release/SDL2_image-devel-2.0.5-VC.zip) (for building from source)
+- [SDL Image Runtime Binaries](https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.0.5-win32-x64.zip) (for distribution of pre-compiled binary)
--- src/sdl_test.c
+++ src/sdl_test.c
@@ -3,51 +3,55 @@
 #include <stdlib.h>
 
 #include <SDL.h>
+#include <SDL_image.h>
+
 #ifdef _WIN32
 #undef main
 #endif
 
-#include "data/data.h"
+// ============================================================================
+// Global definitions
+// ============================================================================
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
+const char* spaceship_img = "data/ship.png";
+
 typedef struct {
     SDL_Window* window;
-    SDL_Surface* window_surface;
-    SDL_Surface* spaceship_surface;
+    SDL_Renderer* renderer;
+    SDL_Texture* spaceship_texture;
 } game_state_t;
 
 game_state_t state;
 
-void init_game_state()
-{
-    state.window = NULL;
-    state.window_surface = NULL;
-    state.spaceship_surface = NULL;
-}
+// ============================================================================
+// Forward declarations
+// ============================================================================
 
-void destroy_game_state()
-{
-    SDL_FreeSurface(state.spaceship_surface);
-    SDL_FreeSurface(state.window_surface);
-    if(state.window == NULL) {
-        SDL_DestroyWindow(state.window);
-    }
-}
+void init();
+void destroy();
+void render();
+
+// ============================================================================
+// Function implementations
+// ============================================================================
 
 void init()
 {
+    state.window = NULL;
+    state.renderer = NULL;
+    state.spaceship_texture = NULL;
+
     if(SDL_Init(SDL_INIT_VIDEO) < 0) {
         fprintf(stderr, "SDL failed to initialise: %s\n", SDL_GetError());
         exit(EXIT_FAILURE);
     }
 
-    init_game_state();
-
     state.window = SDL_CreateWindow(
         "sdl-test",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         SCREEN_WIDTH, SCREEN_HEIGHT,
         SDL_WINDOW_SHOWN
     );
@@ -56,44 +60,76 @@ void init()
         exit(EXIT_FAILURE);
     }
 
-    state.window_surface = SDL_GetWindowSurface(state.window);
-    SDL_FillRect(state.window_surface, NULL, SDL_MapRGB(state.window_surface->format, 0x0, 0x0, 0x0));
+    state.renderer = SDL_CreateRenderer(state.window, -1, SDL_RENDERER_ACCELERATED);
+    if(state.renderer == NULL) {
+        fprintf(stderr, "SDL renderer could not be created: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
 
-    SDL_RWops* rw = SDL_RWFromConstMem(spaceship_bitmap, sizeof(spaceship_bitmap));
-    state.spaceship_surface = SDL_LoadBMP_RW(rw, 1);
+    SDL_SetRenderDrawColor(state.renderer, 0x0, 0x0, 0x0, 0xFF);
+
+    int imgFlags = IMG_INIT_PNG;
+    if(!(IMG_Init(imgFlags) & imgFlags)) {
+        fprintf(stderr, "SDL image could no be initialised: %s\n", IMG_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Surface* spaceship_surface = IMG_Load(spaceship_img);
+    if(spaceship_surface == NULL) {
+        fprintf(stderr, "Failed to load image from: \"%s\": %s\n", spaceship_img, IMG_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    state.spaceship_texture = SDL_CreateTextureFromSurface(state.renderer, spaceship_surface);
+    if(state.spaceship_texture == NULL) {
+        fprintf(stderr, "SDL texture could not be created: %s\n", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_FreeSurface(spaceship_surface);
 }
 
 void destroy()
 {
-    destroy_game_state();
+    SDL_DestroyTexture(state.spaceship_texture);
+    state.spaceship_texture = NULL;
+
+    SDL_DestroyRenderer(state.renderer);
+    state.renderer = NULL;
+
+    SDL_DestroyWindow(state.window);
+    state.window = NULL;
+
+    IMG_Quit();
+    SDL_Quit();
 }
 
 void render()
 {
-    SDL_Rect dest_coords = {
-        .x = SCREEN_WIDTH / 2,
-        .y = SCREEN_HEIGHT / 2
-    };
-    SDL_BlitSurface(state.spaceship_surface, NULL, state.window_surface, &dest_coords);
+    SDL_RenderClear(state.renderer);
 
-    SDL_UpdateWindowSurface(state.window);
+    SDL_RenderCopy(state.renderer, state.spaceship_texture, NULL, NULL);
+
+    SDL_RenderPresent(state.renderer);
 }
+
+// ============================================================================
+// Main entry point
+// ============================================================================
 
 int main(int argc, char* argv[])
 {
     init();
 
-    for(;;) {
+    bool running = true;
+    SDL_Event event;
+    while(running) {
         // Poll for events
-        bool running = true;
-        SDL_Event event;
         while(SDL_PollEvent(&event)) {
             if(event.type == SDL_QUIT) {
                 running = false;
             }
         }
-        if(!running)
-            break;
 
         // Updated game state
 
@@ -104,8 +140,6 @@ int main(int argc, char* argv[])
     }
 
     destroy();
-
-    SDL_Quit();
 
     return EXIT_SUCCESS;
 }
